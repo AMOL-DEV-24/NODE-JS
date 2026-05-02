2741520d659512a0b1f5f136a1802c83602ad6eb
Amol Pawar <amolpawar.dev@gmail.com>
2026-05-02 11:17:13 +0530

    Added the Es6 Modules code

---
+++ ES6 MODULES/IMPORTING/DEFAULT IMPORTS/addDefaultIndex.js
@@ -0,0 +1,5 @@
+
+import add from '../../EXPORTING/TYPES/DEFAULT EXPORTS/addDefault.js'
+
+
+console.log(add(10, 20));
---
+++ ES6 MODULES/IMPORTING/NAMED IMPORTS/addNamedIndex.js
@@ -0,0 +1,4 @@
+import {add, sub} from '../../EXPORTING/TYPES/NAMED EXPORTS/addNamed.js'
+
+console.log(add(3, 2));
+
