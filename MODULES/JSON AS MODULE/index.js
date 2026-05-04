5031e8ed434e33f421ea63417e139404389e0d19
Amol Pawar <amolpawar.dev@gmail.com>
2026-05-04 16:33:19 +0530

    Added the Modules Code

---
+++ MODULES/JSON AS MODULE/index.js
@@ -0,0 +1,10 @@
+
+const data = require("./data.json");
+
+console.log("name: ",data.name);
+console.log("city: ",data.city);
+console.log("skills: ",data.skills.at(3));
+
+console.log("Data: ",data);
+
+
